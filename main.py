fc6e1de199637ddc7c43fd6a13f8dd99430162e2
Zaid <hamwizaid@gmail.com>
2025-11-12 21:31:17 +0400

    Set up

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 from PySide6 import QtWidgets
 from ui_main import App
 
-
-app = QtWidgets.QApplication(sys.argv)
-launcher = App()
-launcher.show()
-sys.exit(app.exec())
+if __name__ == '__main__':
+    print(__name__)
+    app = QtWidgets.QApplication(sys.argv)
+    launcher = App()
+    launcher.show()
+    sys.exit(app.exec())
