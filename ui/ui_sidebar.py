9da21ae6c4839b32e073925bcfe58aae4b74074d
Zaid <hamwizaid@gmail.com>
2025-11-24 14:38:54 +0400

    Prototyping - Cleaning

--- ui/ui_sidebar.py
+++ ui/ui_sidebar.py
@@ -14,25 +14,20 @@ class Sidebar(QWidget):
         layout = QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
 
-        # ---- replace direct layout with your scroll widget ----
         self.scroll_area = VScrollWidget(
             item_spacing=0,
             h_scroll_bar=False,
             v_scroll_bar=True,
             container_name="sidebar_container"
         )
-        layout.addWidget(self.scroll_area)  # sidebar is now scrollable
+        layout.addWidget(self.scroll_area)
         self.scroll_area.add_spacer(10, 10)
 
-        # -----------------------------------------
-        # BUTTON LIST
-        # -----------------------------------------
         buttons = {
             "Home": "home",
             "Settings": "settings"
         }
 
-        # ---- exclusive checked behavior ----
         self.btn_group = QButtonGroup(self)
         self.btn_group.setExclusive(True)
 
@@ -65,15 +60,16 @@ class Sidebar(QWidget):
 
         self.button_map = {}
 
-        # -----------------------------------------
         # ADD BUTTONS INTO THE SCROLLING LAYOUT
-        # -----------------------------------------
         for label, name in buttons.items():
             btn = QPushButton(label)
             btn.setCheckable(True)
             btn.clicked.connect(lambda checked, n=name: self.select_page(n))
 
             self.scroll_area.add_widget(btn)
+            if str(name) == "settings":
+                print("ADDING STRETCH")
+
             self.btn_group.addButton(btn)
             self.button_map[name] = btn
 
@@ -81,12 +77,10 @@ class Sidebar(QWidget):
 
         self.scroll_area.add_stretch()
 
-        # default selection
+        # default selection todo: allow user to decide default page through settings...
         self.select_page("home")
 
-    # -----------------------------------------
-    # highlight + emit page name
-    # -----------------------------------------
+
     def select_page(self, name: str):
         btn = self.button_map[name]
         btn.setChecked(True)
